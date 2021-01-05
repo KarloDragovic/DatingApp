441ff44006ea9bcd1d006999b0755568491129af
Karlo Dragovic <karlo.dragovic@gmail.com>
2021-01-05 16:43:37 +0100

    Initial Commit

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DatingApp.API.Data 
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+        }
+        public DbSet<Value> Values{ get; set; }
+
+    }
+}
